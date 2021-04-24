7c0b523673180d6a1365748a9732dd14fac40820
Vijay Rangan <vijay.rangan@gmail.com>
2021-04-24 22:24:29 +0530

    fix invalid ref

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "twig");
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
